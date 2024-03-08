ee718e7afe0614419ff0f989267f8c9402b41250
Honeydapaton1 <honeydapaton@gmail.com>
2024-03-08 08:55:41 +0800

    Added Card Element

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+    <IonCard>
+    <img alt="Silhouette of mountains" src="../src/assets/img/love.jpg" />
+    <IonCardHeader>
+      <IonCardTitle>Honey Grace Dapaton </IonCardTitle>
+      <IonCardSubtitle>Student</IonCardSubtitle>
+    </IonCardHeader>
+
+    <IonCardContent>Nothing more, nothing less.</IonCardContent>
+  </IonCard>
+);
+}
 
 export default Tab1;
