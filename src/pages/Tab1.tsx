e38ed0ac3cddc2b4736be7541566ea7e3d74e461
Honeydapaton1 <honeydapaton@gmail.com>
2024-03-19 08:12:48 +0800

    Added button and Alert functions

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,  IonCard, IonCardContent, IonCardHeader, 
+          IonCardSubtitle, IonCardTitle, IonAlert, IonButton,  IonActionSheet} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -12,6 +13,46 @@ const Tab1: React.FC = () => {
     </IonCardHeader>
 
     <IonCardContent>Nothing more, nothing less.</IonCardContent>
+
+    <IonButton id="present-alert" expand="block">Quick Facts </IonButton>
+      <IonAlert
+        trigger="present-alert"
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={['Action']}
+      ></IonAlert>
+    
+    <IonButton id="open-action-sheet" expand="block">Open Action Sheet </IonButton>
+    <IonActionSheet
+        trigger="open-action-sheet"
+        header="Actions"
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+          {
+            text: 'Share',
+            data: {
+              action: 'share',
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            data: {
+              action: 'cancel',
+            },
+          },
+        ]}
+      ></IonActionSheet>
+
+
+
   </IonCard>
 );
 }
