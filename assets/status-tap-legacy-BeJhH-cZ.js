51009213ca70aa713e2930e50d75311a7adf3fec
HoneyDapaton1 <honeydapaton@gmail.com>
2024-05-10 19:02:23 +0800

    Updates

--- assets/status-tap-legacy-BeJhH-cZ.js
+++ assets/status-tap-legacy-BeJhH-cZ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Di62-wM-.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-Bcmahjq8.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
