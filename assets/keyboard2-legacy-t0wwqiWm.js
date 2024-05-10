e8be712e89e27387484c492009cd9a455a2df4ca
HoneyDapaton1 <honeydapaton@gmail.com>
2024-05-10 19:05:37 +0800

    Updates

--- assets/keyboard2-legacy-t0wwqiWm.js
+++ assets/keyboard2-legacy-t0wwqiWm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bcmahjq8.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DKWujs6Q.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
